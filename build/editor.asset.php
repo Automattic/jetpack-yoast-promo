46280816fc29b34d3e9e823d5832fc1aa47cf7e1
sergeymitr <sergeymitr@users.noreply.github.com>
2023-07-18 00:54:13 +0000

    Connection: redirect users to proper Calypso environment during connection (#31906)
    - Connection flow: redirect users to the Calypso environment chosen by developer instead of production.
    - Add calypsoEnv into Connection package initial state, use it in jp-redirect component instead of
    
    ---------
    
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5581981929

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '9a9519acb251c1a8d4e2');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'f04c2c1f9e6901a93a01');
