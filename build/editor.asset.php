7aa49e9f39740214f96160849bfe36b4f9418690
lezama <lezama@users.noreply.github.com>
2023-08-07 13:08:21 +0000

    Use shared component for editor panel icons (#32257)
    ---------
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5785234273

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'f04c2c1f9e6901a93a01');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'a532f4bf34a525753b6f');
