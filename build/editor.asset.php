df4ab81fff380220f79b8a088fcaa8394c27581b
andrii-lysenko <andrii-lysenko@users.noreply.github.com>
2024-01-04 20:04:55 +0000

    Add modules store tests (#34835)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7414147948

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '1ea8d966f45edad3040b');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'b0dd82aaa0126f587649');
