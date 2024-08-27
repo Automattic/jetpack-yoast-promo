e42447794faf2ef957836566ecf516f984da3395
anomiex <anomiex@users.noreply.github.com>
2024-08-27 18:39:57 +0000

    eslint: Enable more temporarily-disabled rules (#39057)
    * `no-undef-init`: One instance fixed.
    * `no-prototype-builtins`: Swapped a lot of calls to `.hasOwnProperty()`
      with `Object.hasOwn()`, since all browsers we support appear to
      support that.
    * `no-unused-expressions`: Enabled, but with `allowShortCircuit` and
      `allowTernary` set true since we have a lot of that. Fixing these
      caught bugs in two different places!
    
    Also, moved the `eqeqeq` override ignoring `== null` from "temporarily
    disabled" to "maybe we want to keep this override permanently", to be
    decided later. Put the `no-unused-expressions` allowing of short-circuit
    and ternarys there too.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10583749686
    
    Upstream-Ref: Automattic/jetpack@62905b84d39d3d0f13c43ac1bcb5c76a43ef154e

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'fb6630019f1eacbe4cde');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'ee478842fc7bca52241b');
