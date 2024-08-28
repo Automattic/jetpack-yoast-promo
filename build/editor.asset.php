2252f03d6973328f59be9fafe971b0663d2ee53f
anomiex <anomiex@users.noreply.github.com>
2024-08-28 14:10:49 +0000

    Update dependency webpack to v5.94.0 [SECURITY] (#39111)
    * Update dependency webpack to v5.94.0 [SECURITY]
    * i18n-check-webpack-plugin: Update line numbers in test snapshot
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10598144482
    
    Upstream-Ref: Automattic/jetpack@8fca14023143e7e1a81868136867c0754dfa0ad8

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'ee478842fc7bca52241b');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '75dad81ac985a31fb73d');
