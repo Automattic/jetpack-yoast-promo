6febfe2057914329f053edd3cfc4852efa44201a
tbradsha <tbradsha@users.noreply.github.com>
2025-01-01 21:07:32 +0000

    Update dependency debug to v4.4.0 (#40810)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12574297679
    
    Upstream-Ref: Automattic/jetpack@df2c96ecba4d83d71b72c154623f2da42acb6592

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-connection', 'lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => '8e2f43f3be5122eb7867');
+<?php return array('dependencies' => array('jetpack-connection', 'lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'a062ad00eef3c6311e02');
